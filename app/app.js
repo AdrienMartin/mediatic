ed75a734e1e037de9b787daa7483026cae00972a
ali <ali@live.fr>
2016-04-12 11:05:17 +0200

    ajout index.html et module-global

---
+++ app/app.js
@@ -0,0 +1,22 @@
+
+// Création du module ModuleApp avec les dépendances :
+//  - ng-route : pour gérer des URL différentes
+//  - ModuleMedia : pour gérer le catalogue de l'application
+//  - ModuleAdherent : pour gérer les livres de l'application
+//  - ModuleGlobal : pour avoir les filtres
+angular.module('ModuleApp', ['ngRoute']);
+
+// Configuration du module ModuleApp
+// => Injection du Provider du service $route afin de le configurer.
+angular.module('ModuleApp').config(function($routeProvider){
+	
+		$routeProvider.when('/login',{
+			templateUrl:'module-global/templates/login.html',
+			controller:'ConnexionController',
+			controllerAs:'conCtrl'
+		});
+		$routeProvider.otherwise({
+			redirectTo : '/login'
+		})
+});
+
