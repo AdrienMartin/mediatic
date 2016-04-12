d0132747d146138a828d82e0dafe1d6e42598fcf
adrien <adrien.mtn@gmail.com>
2016-04-12 16:34:05 +0200

    ajout recherche

--- app/module-adherent/js/controller/RechercheAdherentController.js
+++ app/module-adherent/js/controller/RechercheAdherentController.js
@@ -1,5 +1,5 @@
 
-angular.module('ModuleAdherent').controller('RechercheAdherentController', ['$http', '$rootScope', 'RechercheAdherentService', function($http, $rootScope, RechercheAdherentService){
+angular.module('ModuleAdherent').controller('RechercheAdherentController', ['$http', '$rootScope', 'RechercheAdherentService', '$scope', function($http, $rootScope, RechercheAdherentService, $scope){
 	var myCtrl = this;
 	
 	// Je défini l'attribut PAGE pas si il n'ai pas déjà défini
@@ -8,6 +8,7 @@ angular.module('ModuleAdherent').controller('RechercheAdherentController', ['$ht
 	$rootScope.page.titre = "Recherche Adhérent";
 	
 	myCtrl.listeAdherents = undefined;
+	myCtrl.filter = myCtrl.filter || {};
 	
 	RechercheAdherentService.getListe().then(function(response) {
 		myCtrl.listeAdherents = response;
@@ -18,6 +19,21 @@ angular.module('ModuleAdherent').controller('RechercheAdherentController', ['$ht
 	
 	myCtrl.hasErrorAdherent = function(){
 		return myCtrl.listeAdherents===-1;
+	}
+	
+	myCtrl.rechercher = function()
+	{
+		var params = {};
+		if (myCtrl.filter.id != undefined) {
+			params.id = myCtrl.filter.id;
+		}
+		
+		RechercheAdherentService.getListe(params).then(function(response) {
+			myCtrl.listeAdherents = response;
+		}, function(){
+			// En cas d'erreur
+			myCtrl.listeAdherents = -1;
+		});
 	}
 	
 }]);
--- app/module-adherent/js/service/adherents.js
+++ app/module-adherent/js/service/adherents.js
@@ -2,39 +2,31 @@ angular.module('ModuleAdherent').service('RechercheAdherentService', ['$http', '
 {
     var self = this;
     var url = "http://10.34.10.140:8080/resource/adherent.recherche";
-    var promise = undefined;
-
-    var initPromise = function()
-    {
-        if(promise == undefined)
-        {
-            promise = $http.get(url).then(function(response)
-            {
-                var adherents = [];
-                for(var index in response.data)
-                {
-                    var itemFromServeur = response.data[index];
-                    var itemForIHM = {
-                            id : itemFromServeur.id,
-                            nom : itemFromServeur.nom,
-                            prenom : itemFromServeur.prenom,
-                            date_naissance : new Date(itemFromServeur.date_naissance),
-                            cotisation_correcte : itemFromServeur.cotisation_correcte,
-                            emprunt : itemFromServeur.nombre_media
-                    };
-           
-                    adherents.push(itemForIHM);
-                }
-                return adherents;
-            });
-        }
-    }
     
-    self.getListe = function()
+    self.getListe = function(params)
     {
-        initPromise();
-        return promise;
+    	return $http.get(url,{params:params}).then(function(response)
+        {
+            var adherents = [];
+            for(var index in response.data)
+            {
+                var itemFromServeur = response.data[index];
+                var itemForIHM = {
+                        id : itemFromServeur.id,
+                        nom : itemFromServeur.nom,
+                        prenom : itemFromServeur.prenom,
+                        date_naissance : new Date(itemFromServeur.date_naissance),
+                        cotisation_correcte : itemFromServeur.cotisation_correcte,
+                        emprunt : itemFromServeur.nombre_media
+                };
+       
+                adherents.push(itemForIHM);
+            }
+            return adherents;
+        });
+        
     }
+  
     
 }]);
 
