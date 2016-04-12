253998023389c1bce5caa793a5f03850095668e9
toto <you@example.com>
2016-04-12 14:29:22 +0200

    service media

---
+++ app/module-media/js/service/listeMedia.js
@@ -0,0 +1,39 @@
+angular.module('ModuleMedia').service('ListeMediaService', ['$http',function($http)
+{
+	var self = this;
+	var url = "http://10.34.10.140:8080/resource/media.recherche";
+	var promise = undefined;
+
+	var initPromise = function()
+	{
+		if(promise == undefined)
+		{
+			promise = $http.get(url).then(function(response)
+			{
+				var medias = [];
+				for(var index in response.data)
+				{
+					var itemFromServeur = response.data[index];
+					var itemForIHM =
+					{
+							id : itemFromServeur.id,
+							titre : itemFromServeur.titre,
+							auteur : itemFromServeur.auteur,
+							type : itemFromServeur.type,
+							emprunteur : itemFromServeur.emprunteur
+					};
+					medias.push(itemForIHM);
+				}
+
+				console.log(medias);
+				return medias;
+			});
+		}
+	}
+	
+	self.getListe = function()
+	{
+		initPromise();
+		return promise;
+	}
+}]);
